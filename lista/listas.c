e6afaf7726574c98a09564111cac43ad3930f2ee
hideaki-san <lucashideaki07@gmail.com>
2022-09-06 15:28:38 -0300

    correcao de erros

--- lista/listas.c
+++ lista/listas.c
@@ -24,18 +24,21 @@ dados->prox = NULL;
 return dados; 
 }
 
-void listaIniciar(int valor)
+list *listaIniciar(int valor)
 {
-list *listaEnc =(list *)calloc(1, sizeof(list));
-listaEnc->ref = infoCriar(valor);
-listaEnc->size++;
+list *L =(list *)calloc(1, sizeof(list));
+L->ref = infoCriar(valor);
+L->size++;
+
+return L;
 }
 
 void listaAddInicio(list *L, int valor)
 {
 if(L == NULL)
   {
-  listaIniciar(valor);
+  L = listaIniciar(valor);
+  return;
   }
   else
   {
@@ -51,7 +54,7 @@ void listaAddFinal(list *L, int valor)
 {
 if(L == NULL)
   {
-  listaIniciar(valor);
+  L = listaIniciar(valor);
   }
   else
   {
@@ -65,41 +68,72 @@ if(L == NULL)
 
 void listaAddMeio(list *L, int pos, int valor)
 {
+if(L->size < pos)
+  {
+  printf("\nINSERIDO NO FINAL DA LISTA(posicao nao existe)\n");
+  listaAddFinal(L, valor);
+  }
 if(L == NULL)
   {
-  listaIniciar(valor);
+  L = listaIniciar(valor);
+  return;
   }
   else
   {
-  info *aux = L->ref, *marcador;
+  info *aux = L->ref;
   int i=1;
   
-  while(i < pos)
+  while(i < (pos-1))
     {
     aux = aux->prox;
     
     if(aux->prox == NULL)
       {
-      info *dados =(info *)calloc(1, sizeof(info));
-      dados->valor = valor;
-      dados->prox = NULL;
+      info *dados =infoCriar(valor);
       aux->prox = dados;  
       L->size++;
       return;
       }
-    
-    if(i == pos-1)
-      {
-      marcador = aux;
-      }
-    
     i++;
     }
 
   info *dados =(info *)calloc(1, sizeof(info));
   dados->valor = valor;
-  dados->prox = aux;
-  marcador->prox = dados;  
+  dados->prox = aux->prox;
+  aux->prox = dados;  
   L->size++;
   }
+}
+
+void listaAddOrdenado(list *L, int valor)
+{
+if(L == NULL)
+  {
+  L = listaIniciar(valor);
+  }
+info *aux = L->ref;
+
+while(aux->valor > valor)
+  {
+  if(aux->prox->valor < valor)
+    {
+    info *dados =(info *)calloc(1, sizeof(info));
+    dados->valor = valor;
+    dados->prox = aux->prox;
+    aux->prox = dados;
+    }
+  aux = aux->prox;
+  }
+}
+
+void listaImprimir(list *L)
+{
+info *aux = L->ref;
+printf("\nTAMANHO: %d\n", L->size);
+while(aux != NULL)
+  {
+  printf("[%d]->", aux->valor);
+  aux = aux->prox;
+  }
+printf("[NULL]");
 }
--- lista/listas.h
+++ lista/listas.h
@@ -5,12 +5,12 @@ typedef struct INFO info;
 typedef struct LIST list;
 
 info *infoCriar(int valor);
-void listaIniciar();
+list *listaIniciar(int valor);
 void listaAddInicio(list *L, int valor);
 void listaAddFinal(list *L, int valor);
 void listaAddMeio(list *L, int pos, int valor);
 void listaImprimir(list *L);
 
-//void listaAddOrdenado(list *L, int valor)
+void listaAddOrdenado(list *L, int valor);
 
 #endif
